9ea399effc5178a684f44f364d2dbbde39785f32
PandaDuMetro <thomas.briquet@isen-lille.fr>
2018-01-10 10:47:59 +0100

    ajout du premier css

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var video = videojs('player',{
+    
+    hotkeys: {
+        
+        seekStep : 10,
+        volumeStep : 0.2,
+        enableNumbers : false,
+        enableMute : true,
+        enableFullscreen : true,
+    }
+    
+});
+
+
+
+ 
